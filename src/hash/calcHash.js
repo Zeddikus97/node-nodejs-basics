df05770b95c353e9a0cceb372bac4216603b8188
Pavel <opatriarkh@gmail.com>
2023-06-19 21:56:45 +0300

    feat: add hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import { readFile } from 'fs/promises';
+import { createHash } from 'crypto';
+import __dirname from './pathGenerator/pathGenerator.js';
+
 const calculateHash = async () => {
-    // Write your code here 
+    await readFile(`${__dirname}files/fileToCalculateHashFor.txt`, () => {
+        //console.log(success)
+    })
+    .then(fileData => {
+        console.log(createHash('sha256').update(fileData).digest('hex'));
+    })
+    .catch(err => {
+        throw err;
+    })
 };
 
 await calculateHash();
