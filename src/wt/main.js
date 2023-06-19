21f86ef3f90a6de909bc6686de5f17ae26601f90
Pavel <opatriarkh@gmail.com>
2023-06-19 23:47:36 +0300

    feat: add wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,31 @@
+
+import { Worker } from "worker_threads";
+import { cpus } from "os";
+import __dirname from './pathGenerator/pathGenerator.js';
+
+
 const performCalculations = async () => {
-    // Write your code here
+    const procs = cpus();
+    //console.log(procs);
+
+    const results = await Promise.allSettled(procs.map((proc, index)=>{
+        console.log(index, "index")
+        return new Promise((res, rej)=> {
+            const worker = new Worker(`${__dirname}worker.js`, {
+                workerData: index+10
+            })
+            worker.on("message", msg => res(msg))
+            worker.on("error", err => rej(err))
+        })
+    }))
+
+    const output = results.map(({status, value}) =>(
+        {
+            status : status==="fulfilled" ? "resolved" : "error",
+            data: status==="fulfilled" ? value : null
+        }
+    ))
+    console.log(output)
 };
 
 await performCalculations();
