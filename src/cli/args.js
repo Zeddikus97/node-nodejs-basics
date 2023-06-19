7b98ce86c33aed0076c599bf6d7657dc9d87dee4
Pavel <opatriarkh@gmail.com>
2023-06-19 21:41:23 +0300

    feat: add cli, fixed missed feature

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
-const parseArgs = () => {
+const parseArgs = async () => {
+    let lines = process.argv.slice(2).reduce((lines, arg, index, array)=>{
+        const nextValue = array[index+1]
+        if(arg.split("--").length>1&&nextValue){
+            lines.push(`${arg.slice(2)} is ${nextValue}`);
+        }
+        return lines;
+    }, [])
+    console.log(lines.join(", "))
     // Write your code here 
 };
 
-parseArgs();
+await parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
-const parseEnv = () => {
+const parseEnv = async () => {
+    let variables_array=[];
+    let index=1;
+    for(let variable in process.env){
+        if(variable.split("RSS_").length>1){
+            variables_array.push(`RSS_name${index}=${process.env[variable]}`);
+            index++;
+        }
+    }
+    console.log(variables_array.join("; "))
     // Write your code here 
 };
 
-parseEnv();
+await parseEnv();
