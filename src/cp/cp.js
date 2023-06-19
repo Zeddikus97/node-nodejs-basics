130d9580a1e69e9544b544e32a78b3c9b78db8a7
Pavel <opatriarkh@gmail.com>
2023-06-20 00:11:25 +0300

    feat: add cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,9 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import { fork } from "child_process"
+import __dirname from './pathGenerator/pathGenerator.js';
+
+export const spawnChildProcess = async (args) => {
+    fork(`${__dirname}files/script.js`, [...args.split(' ')]);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess("--some-arg value1 --other 1337 --arg2 42");
